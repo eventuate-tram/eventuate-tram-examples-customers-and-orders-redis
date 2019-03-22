ac5ee03910a6b63d6f2fb25999681ed0113a107c
Chris Richardson <chris@chrisrichardson.net>
2019-03-22 12:55:49 -0700

    Fixed issues with Java development image

--- publish-docker-images.sh
+++ publish-docker-images.sh
@@ -2,18 +2,13 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-DOCKER_IMAGE_TAG=$BRANCH ./gradlew javaDevelopmentComposeBuild
-
-if ! [[  $BRANCH =~ ^[0-9]+ ]] ; then
-  echo Not release $BRANCH - no PUSH
-  exit 0
-fi
-
-VERSION=$BRANCH
-
-echo Publishing $BRANCH
+export DOCKER_IMAGE_TAG=latest
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-DOCKER_IMAGE_TAG=$BRANCH ./gradlew javaDevelopmentComposePush
+./gradlew javaDevelopmentComposePull || echo no image to pull
+
+./gradlew javaDevelopmentComposeBuild
+
+./gradlew javaDevelopmentComposePush
 
