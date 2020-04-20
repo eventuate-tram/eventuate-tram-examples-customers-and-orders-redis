810c400019bd5f8c45b3790f510bb6337198e713
Chris Richardson <chris@chrisrichardson.net>
2020-04-20 15:16:41 -0700

    Updated LICENSE

--- redis-cli.sh
+++ redis-cli.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -e
 
 docker run ${DOCKER_RUN_OPTIONS:--i} --rm \
-   -e DOCKER_HOST_IP=redis \
    --network=${PWD##*/}_default \
-   redis:5.0.3 bash -c 'redis-cli -h $DOCKER_HOST_IP'
+   redis:5.0.3 bash -c 'redis-cli -h redis'
