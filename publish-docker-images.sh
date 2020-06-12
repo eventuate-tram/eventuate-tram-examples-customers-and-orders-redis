63ac366de1c3315c8a8c4a0225a39c76fa6e7d03
Chris Richardson <chris@chrisrichardson.net>
2020-06-12 07:16:10 -0700

    Switched to base image, buildAndTestAll task now stops containers

--- publish-docker-images.sh
+++ publish-docker-images.sh
@@ -2,6 +2,11 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+if [ "$BRANCH" != "master" ] ; then
+  echo Not master
+  exit 0
+fi
+
 export DOCKER_IMAGE_TAG=latest
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
