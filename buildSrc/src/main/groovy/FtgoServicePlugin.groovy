b31ebdbd233dbd5d3dee0eccae7f6087b43ae149
Chris Richardson <chris@chrisrichardson.net>
2022-06-11 10:31:51 +0900

    Disable Eventuate Spring Cloud Sleuth integration #17

--- buildSrc/src/main/groovy/FtgoServicePlugin.groovy
+++ buildSrc/src/main/groovy/FtgoServicePlugin.groovy
@@ -22,7 +22,8 @@ class FtgoServicePlugin implements Plugin<Project> {
             compile 'org.springframework.cloud:spring-cloud-starter-zipkin'
             compile 'io.zipkin.brave:brave-bom:4.17.1'
 
-            compile "io.eventuate.tram.core:eventuate-tram-spring-cloud-sleuth-integration"
+            // temporarily disable
+            // compile "io.eventuate.tram.core:eventuate-tram-spring-cloud-sleuth-integration"
 
             compile "org.springframework.data:spring-data-redis:2.2.0.M1"
             compile 'io.lettuce:lettuce-core:5.1.4.RELEASE'
