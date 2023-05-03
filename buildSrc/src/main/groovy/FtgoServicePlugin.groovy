3e6fd5a3cd1603184fd9318815ddae14b4c90923
Chris Richardson <chris@chrisrichardson.net>
2023-05-03 09:05:08 -0700

    Upgraded some dependencies

--- buildSrc/src/main/groovy/FtgoServicePlugin.groovy
+++ buildSrc/src/main/groovy/FtgoServicePlugin.groovy
@@ -7,20 +7,9 @@ class FtgoServicePlugin implements Plugin<Project> {
     void apply(Project project) {
 
         project.apply(plugin: 'org.springframework.boot')
-    	project.apply(plugin: "io.spring.dependency-management")
-
-        project.dependencyManagement {
-            imports {
-                mavenBom "org.springframework.cloud:spring-cloud-sleuth:${project.ext.springCloudSleuthVersion}"
-            }
-        }
 
 
         project.dependencies {
-
-            compile 'org.springframework.cloud:spring-cloud-starter-sleuth'
-            compile 'org.springframework.cloud:spring-cloud-starter-zipkin'
-            compile 'io.zipkin.brave:brave-bom:4.17.1'
 
             implementation "org.springframework.cloud:spring-cloud-starter-sleuth"
             implementation "org.springframework.cloud:spring-cloud-sleuth-zipkin"
